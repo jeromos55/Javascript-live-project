486481c20e08303e0a6d6ab48b5042afe713bfdd
jeromos55 <jeromos55@gmail.com>
2023-10-29 17:13:52 +0100

    uődate modal

--- script.js
+++ script.js
@@ -5,9 +5,24 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnOpenModal);
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i].textContent);
+  btnOpenModal[i].addEventListener('click', openModal);
 }
 
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (e) => {
+  console.log(e);
+  if (e.key === 'Escape') closeModal();
+});
