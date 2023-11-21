4dc7835ce5849f551f52037b8fc6de3819d2d18f
jeromos55 <jeromos55@gmail.com>
2023-11-21 21:14:30 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 'use strict';
 
-console.log(message);
-console.log(attorney);
+console.log(this); // window object
 
-var me = 'John';
-let job = 'attorney';
-const year = 1991;
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this); // undefine
+};
+
+calcAge(1991);
+
+// in arrow functions
+
+const calcAgeArrow = (birthYear) => {
+  console.log(2037 - birthYear);
+  console.log(this); // window object
+};
+
+calcAgeArrow(2000);
+
+// in an object
+
+const john = {
+  year: 1990,
+  calcAge: function () {
+    console.log(this); // this object itself
+    console.log(2037 - this.year); // It's 47 becouse this.year = john.year
+  },
+};
+
+john.calcAge();
