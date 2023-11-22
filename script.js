0a317d5c5e8eef7f1025053b68eb5db503bb46d8
jeromos55 <jeromos55@gmail.com>
2023-11-22 15:46:58 +0100

    update

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 'use strict';
 
-console.log(this); // window object
-
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  console.log(this); // undefine
-};
-
-calcAge(1991);
-
-// in arrow functions
-
-const calcAgeArrow = (birthYear) => {
-  console.log(2037 - birthYear);
-  console.log(this); // window object
-};
-
-calcAgeArrow(2000);
-
-// in an object
-
 const john = {
   year: 1990,
+  firstName: 'John',
   calcAge: function () {
-    console.log(this); // this object itself
-    console.log(2037 - this.year); // It's 47 becouse this.year = john.year
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+
+  greet: () => {
+    console.log(`Hey ${this.firstName}`);
   },
 };
 
-john.calcAge();
+john.greet();
+// undefined because the arrow function hasn't 'this' object so it's a global object.
+console.log(this); // golbal object is window object
+console.log(this.firstName); // window object has't 'firstName'
+
+var firstName = 'Maria'; // It has a global scope
+john.greet(); // so that's 'Hey Maria'
