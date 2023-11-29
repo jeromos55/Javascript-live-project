4257aef6d1c081a141481deda74fc3a3a34a55b1
jeromos55 <jeromos55@gmail.com>
2023-11-29 14:13:32 +0100

    update

--- script.js
+++ script.js
@@ -1,123 +1,78 @@
 'use strict';
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 11.33,
-    x: 3.25,
-    team2: 6.5,
+const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day-${2 + 4}`]: {
+    open: 0,
+    close: 24,
   },
 };
 
-/*
------------------------
-   CODING CHALLENGE
------------------------
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  // openingHours: openingHours,
 
-We're building a football betting app (soccer for my American friends)! Suppose we get data from a web service about a certain game ('game' variable on next page). In this challenge we're gonna work with that data.
+  // order: function (starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
 
-Your tasks:
+  // orderDelivery: function ({
+  //   starterIndex = 1,
+  //   mainIndex = 0,
+  //   time = '20:00',
+  //   address,
+  // }) {
+  //   console.log(
+  //     `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+  //   );
+  // },
 
-1. Create one player array for each team (variables 'players1' and 
-'players2')
+  // orderPasta: function (inc1, inc2, inc3) {
+  //   console.log(
+  //     `Here is your delicious pasta with ${inc1}, ${inc2} and ${inc3}`
+  //   );
+  // },
 
-2. The first player in any player array is the goalkeeper and the others are field 
-players. For Bayern Munich (team 1) create one variable ('gk') with the 
-goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 
-field players
+  // orderPizza: function (mainIngredient, ...otherIngredient) {
+  //   console.log(mainIngredient);
+  //   console.log(otherIngredient);
+  // },
 
-3. Create an array 'allPlayers' containing all players of both teams (22 
-players)
+  openingHours, // ES6 enhanced object literals
 
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a 
-new array ('players1Final') containing all the original team1 players plus 
-'Thiago', 'Coutinho' and 'Perisic'
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
-5. Based on the game.odds object, create one variable for each odd (called 
-'team1', 'draw' and 'team2')
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 
-6. Write a function ('printGoals') that receives an arbitrary number of player 
-names (not an array) and prints each of them to the console, along with the 
-number of goals that were scored in total (number of player names passed in)
+  orderPasta(inc1, inc2, inc3) {
+    console.log(
+      `Here is your delicious pasta with ${inc1}, ${inc2} and ${inc3}`
+    );
+  },
 
-7. The team with the lower odd is more likely to win. Print to the console which 
-team is more likely to win, without using an if/else statement or the ternary 
-operator.
-
-Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. 
-Then, call the function again with players from game.scored
-
-GOOD LUCK
-*/
-
-// 1.
-console.log('--- 1. ---');
-const [player1, player2] = game.players;
-console.log(player1, player2);
-
-// 2.
-console.log('--- 2. ---');
-const [gk, ...fieldPlayers] = player1;
-console.log(gk, fieldPlayers);
-
-// 3.
-console.log('--- 3. ---');
-const allPlayers = [...player1, ...player2];
-console.log(allPlayers);
-
-// 4.
-console.log('--- 4. ---');
-const playerFinal = [...player1, 'Thiago', 'Couthino', 'Periscic'];
-console.log(playerFinal);
-
-// 5.
-console.log('--- 5. ---');
-const {
-  odds: { team1, x: draw, team2 },
-} = game;
-console.log(team1, draw, team2);
-
-// 6.
-console.log('--- 6. ---');
-const printGoals = function (...players) {
-  console.log(players);
-  console.log(`${players.length} goals were scored`);
+  orderPizza(mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
 };
-printGoals(...game.scored);
 
-// 7.
-console.log('--- 7. ---');
-team1 < team2 && console.log(`${game.team1} is more likely to win`);
-team1 > team2 && console.log(`${game.team2} is more likely to win`);
+console.log(restaurant);
