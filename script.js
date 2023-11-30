bab9bc6b50ad680560b6770ef55e36c45d062451
jeromos55 <jeromos55@gmail.com>
2023-11-30 16:33:58 +0100

    update

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const openingHours = {
-  [weekdays[3]]: {
+  [weekdays[4]]: {
     open: 12,
     close: 22,
   },
-  [weekdays[4]]: {
+  [weekdays[5]]: {
     open: 11,
     close: 23,
   },
@@ -23,35 +23,8 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  // openingHours: openingHours,
 
-  // order: function (starterIndex, mainIndex) {
-  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  // },
-
-  // orderDelivery: function ({
-  //   starterIndex = 1,
-  //   mainIndex = 0,
-  //   time = '20:00',
-  //   address,
-  // }) {
-  //   console.log(
-  //     `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-  //   );
-  // },
-
-  // orderPasta: function (inc1, inc2, inc3) {
-  //   console.log(
-  //     `Here is your delicious pasta with ${inc1}, ${inc2} and ${inc3}`
-  //   );
-  // },
-
-  // orderPizza: function (mainIngredient, ...otherIngredient) {
-  //   console.log(mainIngredient);
-  //   console.log(otherIngredient);
-  // },
-
-  openingHours, // ES6 enhanced object literals
+  openingHours,
 
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -75,4 +48,30 @@ const restaurant = {
   },
 };
 
-console.log(restaurant);
+console.log(restaurant.openingHours.mon);
+// console.log(restaurant.openingHours.mon.open); // this is error message because itn't exist
+
+if (restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open); // this is not print because above
+}
+
+if (restaurant.openingHours.Fri) {
+  console.log(restaurant.openingHours.Fri.open); // 11 because Fri is exist
+}
+
+if (restaurant.openingHours && restaurant.openingHours.Fri) {
+  console.log(restaurant.openingHours.Fri.open); // 11 because Fri is exist
+}
+
+// WIIH optional chaining
+console.log(restaurant.openingHours.mon?.open); // it's undefined if not exist mon
+console.log(restaurant.openingHours?.mon?.open); // it's undefined if not exist mon and openingHours
+
+// Example
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+for (const day of days) {
+  // console.log(day);
+  //const open = restaurant.openingHours[day]?.open || 'colsed'; // It isn't correct because the 0 is falsly, so we get 'closed' here
+  const open = restaurant.openingHours[day]?.open ?? 'colsed';
+  console.log(`On ${day}, we open at ${open}`);
+}
