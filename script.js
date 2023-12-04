3c98b5125c33a424b3f8f9e646d0133ecc164c3d
jeromos55 <jeromos55@gmail.com>
2023-12-04 15:25:24 +0100

    update

--- script.js
+++ script.js
@@ -1,97 +1,88 @@
 'use strict';
 
-const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-const openingHours = {
-  [weekdays[4]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[5]]: {
-    open: 11,
-    close: 23,
-  },
-  // [`day-${2 + 4}`]: {
-  //   open: 0,
-  //   close: 24,
-  // },
-  [weekdays[`${2 + 4}`]]: {
-    open: 0,
-    close: 24,
-  },
-};
-
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours,
-
-  order(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
-
-  orderPasta(inc1, inc2, inc3) {
-    console.log(
-      `Here is your delicious pasta with ${inc1}, ${inc2} and ${inc3}`
-    );
-  },
-
-  orderPizza(mainIngredient, ...otherIngredient) {
-    console.log(mainIngredient);
-    console.log(otherIngredient);
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
 };
 
-console.log(restaurant.openingHours.mon);
-// console.log(restaurant.openingHours.mon.open); // this is error message because itn't exist
+/*
+-----------------------
+  CODING CHALLENGE
+-----------------------
 
-if (restaurant.openingHours.mon) {
-  console.log(restaurant.openingHours.mon.open); // this is not print because above
+Let's continue with our football betting app! Keep using the 'game' variable from before.
+
+Your tasks:
+
+1. Loop over the game.scored array and print each player name to the console, 
+along with the goal number (Example: "Goal 1: Lewandowski")
+
+2. Use a loop to calculate the average odd and log it to the console (We already 
+studied how to calculate averages, you can go check if you don't remember)
+
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them 
+(except for "draw"). Hint: Note how the odds and the game objects have the 
+same property names
+
+GOOD LUCK */
+
+// 1.
+console.log('--- 1. ---');
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
 }
 
-if (restaurant.openingHours.Fri) {
-  console.log(restaurant.openingHours.Fri.open); // 11 because Fri is exist
-}
+// 2.
+console.log('--- 2. ---');
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
 
-if (restaurant.openingHours && restaurant.openingHours.Fri) {
-  console.log(restaurant.openingHours.Fri.open); // 11 because Fri is exist
-}
-
-// WIIH optional chaining
-console.log(restaurant.openingHours.mon?.open); // it's undefined if not exist mon
-console.log(restaurant.openingHours?.mon?.open); // it's undefined if not exist mon and openingHours
-
-// Example
-const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-for (const day of days) {
-  // console.log(day);
-  //const open = restaurant.openingHours[day]?.open || 'colsed'; // It isn't correct because the 0 is falsely, so we get 'closed' here
-  const open = restaurant.openingHours[day]?.open ?? 'colsed'; // now ?? is coorecting the problem and we get 0 here
-  console.log(`On ${day}, we open at ${open}`);
-}
-
-// Methods
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist!');
-console.log(restaurant.orderXXX?.(0, 1) ?? 'Method does not exist!');
-
-// Arrays
-const users = [{ name: 'John' }, { email: 'hello@john.io' }];
-console.log(users[0]?.name ?? "User array item isn't present!");
-console.log(users[0]?.nameXXX ?? "User array item isn't present!");
-
-// this same as above but there are simple and smart for using for arrays
-if (users.length < 0) {
-  console.log(users[0]?.name);
-} else {
-  console.log("User array item isn't present!");
+// 3.
+console.log('--- 3. ---');
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw ' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
 }
