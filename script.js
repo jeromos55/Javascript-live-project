c05e5e6bd7a1f48df209fb51c600c605836af13f
jeromos55 <jeromos55@gmail.com>
2023-12-05 16:26:23 +0100

    update

--- script.js
+++ script.js
@@ -1,118 +1,69 @@
 'use strict';
 
-const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+/*
+-----------------------
+  CODING CHALLENGE
+-----------------------
 
-const openingHours = {
-  [weekdays[4]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[5]]: {
-    open: 11,
-    close: 23,
-  },
-  // [`day-${2 + 4}`]: {
-  //   open: 0,
-  //   close: 24,
-  // },
-  [weekdays[`${2 + 4}`]]: {
-    open: 0,
-    close: 24,
-  },
-};
+Let's continue with our football betting app! This time, we have a map called 
+'gameEvents' (see below) with a log of the events that happened during the 
+game. The values are the events themselves, and the keys are the minutes in which 
+each event happened (a football game has 90 minutes plus some extra time).
 
-//Maps has key, value pairs but every items can be deifferent types opposite the object items type.
+Your tasks:
 
-console.log('--- Maps ---');
+1. Create an array 'events' of the different game events that happened (no 
+duplicates)
 
-const rest = new Map();
-rest.set('name', 'Clasico Italiano');
-rest.set(1, 'Firenze', 'Italy');
-rest.set(2, 'Lisbon', 'Portugal');
-console.log(rest);
+2. After the game has finished, is was found that the yellow card from minute 64 
+was unfair. So remove this event from the game events log.
 
-const rest2 = new Map();
-rest2
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open. :)')
-  .set(false, 'We are closed. :(');
+3. Compute and log the following string to the console: "An event happened, on 
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
 
-console.log(rest2);
-console.log(rest2.get('categories'));
-console.log(rest2.get(true));
-console.log(rest.get('name'));
+4. Loop over 'gameEvents' and log each element to the console, marking 
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: GOAL
 
-const time = 21;
-console.log(rest2.get(time > rest2.get('open') && time < rest2.get('close')));
+GOOD LUCK */
 
-const time2 = 8;
-console.log(rest2.get(time2 > rest2.get('open') && time2 < rest2.get('close')));
-
-console.log(rest2.has('categories'));
-console.log(rest2.size);
-
-rest.delete(2);
-console.log(rest);
-
-// rest2.clear();
-// console.log(rest2);
-
-rest2.set([1, 2], 'Test');
-console.log(rest2);
-console.log(rest2.get(1));
-console.log(rest2.get(2));
-console.log(rest[(1, 2)]);
-
-const arr = [1, 2];
-rest2.set(arr, 'Test');
-console.log(rest2);
-console.log(rest2.get(arr));
-
-rest2.set(document.querySelector('h1'), 'Heading');
-console.log(rest2);
-
-// Map iteration
-console.log('--- Map iteration ---');
-const question = new Map([
-  ['question', 'Waht is the best programming language in the world?'],
-  [1, 'C'],
-  [2, 'java'],
-  [3, 'javascript'],
-  ['correct', 3],
-  [true, 'correct'],
-  [false, 'try again'],
+const gameEvents = new Map([
+  [17, 'GOAL'],
+  [36, 'Substitution'],
+  [47, 'GOAL'],
+  [61, 'Substitution'],
+  [64, 'Yellow card'],
+  [69, 'Red card'],
+  [70, 'Substitution'],
+  [72, 'Substitution'],
+  [76, 'GOAL'],
+  [80, 'GOAL'],
+  [92, 'Yellow card'],
 ]);
 
-console.log(question);
+// 1.
+console.log('--- 1. ---');
+const events = new Set([...gameEvents.values()]);
+console.log(events);
 
-// Converting object to map
-console.log('--- Converting object to map ---');
-console.log(Object.entries(openingHours));
+// 2.
+console.log('--- 2. ---');
+gameEvents.delete(64);
+console.log(gameEvents);
 
-const HoursMap = new Map(Object.entries(openingHours));
-console.log(HoursMap);
+// 3.
+console.log('--- 3. ---');
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes.`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes.`
+);
 
-// Quizz app
-console.log('--- Quizz app ---');
-console.log(question.get('question'));
-for (const [key, value] of question) {
-  if (typeof key === 'number') {
-    console.log(`Answer: ${key}: ${value}`);
-  }
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
 }
-
-const answer = Number(prompt('Your answer:'));
-console.log(answer);
-
-// const result = answer === question.get('correct') ? 'correct' : 'try again';
-const result = question.get(answer === question.get('correct'));
-console.log(result);
-
-// convert map to array
-console.log('--- Convert map to array ---');
-console.log(question.entries()); // map3
-console.log([...question]); // array of question
-console.log([...question.keys()]); // array of keys
-console.log([...question.values()]); //array of values
