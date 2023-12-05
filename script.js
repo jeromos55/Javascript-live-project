6f257b01e16525b1b95965310dbf904ef71df272
jeromos55 <jeromos55@gmail.com>
2023-12-05 01:09:26 +0100

    update

--- script.js
+++ script.js
@@ -1,88 +1,53 @@
 'use strict';
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+//Maps has key, value pairs but every items can be deifferent types opposite the object items type.
 
-/*
------------------------
-  CODING CHALLENGE
------------------------
+console.log('--- Maps ---');
 
-Let's continue with our football betting app! Keep using the 'game' variable from before.
+const rest = new Map();
+rest.set('name', 'Clasico Italiano');
+rest.set(1, 'Firenze', 'Italy');
+rest.set(2, 'Lisbon', 'Portugal');
+console.log(rest);
 
-Your tasks:
+const rest2 = new Map();
+rest2
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open. :)')
+  .set(false, 'We are closed. :(');
 
-1. Loop over the game.scored array and print each player name to the console, 
-along with the goal number (Example: "Goal 1: Lewandowski")
+console.log(rest2);
+console.log(rest2.get('categories'));
+console.log(rest2.get(true));
+console.log(rest.get('name'));
 
-2. Use a loop to calculate the average odd and log it to the console (We already 
-studied how to calculate averages, you can go check if you don't remember)
+const time = 21;
+console.log(rest2.get(time > rest2.get('open') && time < rest2.get('close')));
 
-3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
-Odd of victory Bayern Munich: 1.33
-Odd of draw: 3.25
-Odd of victory Borrussia Dortmund: 6.5
-Get the team names directly from the game object, don't hardcode them 
-(except for "draw"). Hint: Note how the odds and the game objects have the 
-same property names
+const time2 = 8;
+console.log(rest2.get(time2 > rest2.get('open') && time2 < rest2.get('close')));
 
-GOOD LUCK */
+console.log(rest2.has('categories'));
+console.log(rest2.size);
 
-// 1.
-console.log('--- 1. ---');
-for (const [i, player] of game.scored.entries()) {
-  console.log(`Goal ${i + 1}: ${player}`);
-}
+rest.delete(2);
+console.log(rest);
 
-// 2.
-console.log('--- 2. ---');
-const odds = Object.values(game.odds);
-let average = 0;
-for (const odd of odds) average += odd;
-average /= odds.length;
-console.log(average);
+// rest2.clear();
+// console.log(rest2);
 
-// 3.
-console.log('--- 3. ---');
-for (const [team, odd] of Object.entries(game.odds)) {
-  const teamStr = team === 'x' ? 'draw ' : `victory ${game[team]}`;
-  console.log(`Odd of ${teamStr}: ${odd}`);
-}
+rest2.set([1, 2], 'Test');
+console.log(rest2);
+console.log(rest2.get(1));
+console.log(rest2.get(2));
+console.log(rest[(1, 2)]);
+
+const arr = [1, 2];
+rest2.set(arr, 'Test');
+console.log(rest2);
+console.log(rest2.get(arr));
+
+rest2.set(document.querySelector('h1'), 'Heading');
+console.log(rest2);
