5b187f38c712e40debf7b269eed5598873c289f0
jeromos55 <jeromos55@gmail.com>
2023-12-11 16:53:23 +0100

    update

--- script.js
+++ script.js
@@ -1,50 +1,28 @@
 'use strict';
 
-/*
+//-----------------------------
+// String Methods Practice
 
------------------------
-  CODING CHALLENGE
------------------------
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-Write a program that receives a list of variable names written in underscore_case 
-and convert them to camelCase.
-The input will come from a textarea inserted into the DOM (see code below to 
-insert the elements), and conversion will happen when the button is pressed.
+// 🔴 Delayed Departure from FAO to TXL (11h25) |
+//              Arrival from BRU to FAO (11h45) |
+//   🔴 Delayed Arrival from HEL to FAO (12h05) |
+//            Departure from FAO to LIS (12h30) |
 
-Test data (pasted to textarea, including spaces):
+const getCode = (str) => {
+  return str.slice(0, 3).toUpperCase();
+};
 
-underscore_case
-  first_name
-Some_Variable 
-  calculate_AGE
-delayed_departure
-
-Should produce this output (5 separate console.log outputs):
-
-underscoreCase   ✅
-firstName        ✅✅
-someVariable     ✅✅✅
-calculateAge     ✅✅✅✅
-delayedDeparture ✅✅✅✅✅
-
-Hints:
-
-- Remember which character defines a new line in the textarea
-- The solution only needs to work for a variable made out of 2 words, like a_b
-- Start without worrying about the ✅. 
-  Tackle that only after you have the variable name conversion working 
-- This challenge is difficult on purpose, so start watching the solution in 
-  case you're stuck. Then pause and continue!
-
-Afterwards, test with your own test data!
-
-GOOD LUCK
-
-*/
-
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
-
-document.querySelector('button').addEventListener('click', () => {
-  const text = document.querySelector('textarea').value;
-});
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  console.log(
+    `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+      '_',
+      ' '
+    )} ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(
+      45
+    )
+  );
+}
