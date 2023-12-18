073424d0673080b1c9b64bdf0ce4f79af6d5a99b
jeromos55 <jeromos55@gmail.com>
2023-12-18 20:28:50 +0100

    update

--- script.js
+++ script.js
@@ -1,97 +1,57 @@
 'use strict';
 
-/*
+//-----------------------------------------------
+// Immediately invoked function expression (IIFE)
+//-----------------------------------------------
 
-----------------------
-  Coding Challenge
-----------------------
+const runOnce = function () {
+  console.log('This will never run again.');
+};
+runOnce();
 
-Let's build a simple poll app!
+// IIFE
+(function () {
+  console.log('This will never run again.');
+  const isPrivate = 23;
+  console.log(isPrivate);
+  var notPrivate = 46;
+})();
 
-A poll has a question, an array of options from which people can choose, 
-and an array with the number of replies for each option. This data is stored 
-in the starter 'poll' object below.
+// console.log(isPrivate); // It's will be error message
+// console.log(notPrivate); // It's will be error message too
 
-Your tasks:
+{
+  var isNotPrivate = 35;
+}
 
-1. Create a method called 'registerNewAnswer' on the 'poll' object. 
-The method does 2 things:
-  1.1. Display a prompt window for the user to input the number of the selected option. 
-  The prompt should look like this:
-  What is your favorite programming language?
-    0: JavaScript
-    1: Python
-    2: Rust
-    3: C++
-    (Write option number)
+console.log(isNotPrivate);
 
-  1.2. Based on the input number, update the 'answers' array property. For 
-  example, if the option is 3, increase the value at position 3 of the array by 
+const runOnce2 = () => console.log('This will ALSO never run again.');
+runOnce2();
 
-1. Make sure to check if the input is a number and if the number makes 
-sense (e.g. answer 52 wouldn't make sense, right?)
+// IIFE
+(() => console.log('This will ALSO never run again.'))();
 
-2. Call this method whenever the user clicks the "Answer poll" button.
+//------------
+// Closures
+//------------
 
-3. Create a method 'displayResults' which displays the poll results. The 
-method takes a string as an input (called 'type'), which can be either 'string'
-or 'array'. If type is 'array', simply display the results array as it is, using 
-console.log(). This should be the default option. If type is 'string', display a 
-string like "Poll results are 13, 2, 4, 1".
+const secureBooking = function () {
+  let passengerCount = 0;
 
-4. Run the 'displayResults' method at the end of each 
-'registerNewAnswer' method call.
-
-5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test 
-data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll 
-object! So what should the this keyword look like in this situation?
-The Complete JavaScript Course 21
-
-Test data for bonus:
-
-  Data 1: [5, 2, 3]
-  Data 2: [1, 5, 3, 9, 6, 1]
-
-Hints: Use many of the tools you learned about in this and the last section
-
-GOOD LUCK 
-
-*/
-
-const poll = {
-  question: 'What is your favorite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-  answers: new Array(4).fill(0), // this generates [0, 0, 0, 0]
-  registerNewAnswer() {
-    // Get answer
-    const answer = Number(
-      prompt(
-        `${this.question}\n${this.options.join('\n')}\n(Write your answer)`
-      )
-    );
-    console.log(answer);
-    // Register answer
-    typeof answer === 'number' &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
-    this.displayResult();
-    this.displayResult('string');
-  },
-  displayResult(type = 'array') {
-    if (type === 'array') {
-      console.log(this.answers);
-    } else if (type === 'string') {
-      console.log(`Poll result are ${this.answers.join(', ')}`);
-    }
-  },
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passenger`);
+  };
 };
 
-document
-  .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+const booker = secureBooking();
+console.log(booker); // This function has access and can modify the passengerCount variable
+booker();
+booker();
+booker();
 
-poll.displayResult.call({ answers: [5, 2, 3] });
-poll.displayResult.call({ answers: [5, 2, 3] }, 'string');
+console.dir(booker);
 
-poll.displayResult.call({ answers: [1, 5, 3, 9, 6, 1] });
-poll.displayResult.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+console.log(secureBooking);
+console.dir(secureBooking);
