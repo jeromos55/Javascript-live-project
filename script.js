dfe6b913fe14a90e6dea81e415cc00ac2a3c233b
jeromos55 <jeromos55@gmail.com>
2024-01-19 14:40:11 +0100

    update

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 'use strict';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
-
+//---------------------------------------------
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
@@ -35,6 +32,7 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4];
 
+//---------------------------------------------
 // Elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
@@ -61,16 +59,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+//---------------------------------------------
+// BANKIST APP
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+  // containerMovements.textContent = 0;
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
