699cba7badb50c39afe7cadfae59b7af15e22488
jeromos55 <jeromos55@gmail.com>
2024-03-21 12:55:45 +0100

    update

--- script.js
+++ script.js
@@ -528,3 +528,18 @@ const sliders = function () {
   });
 };
 sliders();
+
+// lifecycle DOM events
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+// document.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log('Leaving the page', e);
+//   e.returnValue = '';
+// });
