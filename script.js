7e4d857310dd1b72a2bb2fe54d258821964b7d9f
jeromos55 <jeromos55@gmail.com>
2024-03-27 22:09:02 +0100

    update

--- script.js
+++ script.js
@@ -77,3 +77,76 @@ Array.prototype.unique = function () {
 };
 
 console.log(arr.unique());
+
+// ES6 Classes
+
+// class expression
+// const PersonCl  = class{};
+
+// class declaration
+
+class PersonCl {
+  constructor(fullName, birthDay) {
+    this.fullName = fullName;
+    this.birthDay = birthDay;
+  }
+
+  // method will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthDay);
+  }
+
+  greet() {
+    console.log(`Hey, ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthDay;
+  }
+
+  // set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey, ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class
+// 3. Classes are executed in strict mode
+
+const walter = new PersonCl('Walter White', 1960);
+
+const account = {
+  owner: 'John',
+  movement: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movement.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movement.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movement);
