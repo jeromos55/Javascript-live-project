292981b52d4360f23586714e35335569be3a38f3
jeromos55 <jeromos55@gmail.com>
2024-05-30 08:27:56 +0200

    update

--- script.js
+++ script.js
@@ -293,6 +293,41 @@ const renderCountry = function (data, className = '') {
 
 //--------------------------------------------------
 
+// const getJSON = function (url, errorMsg = 'Something went wrong') {
+//   return fetch(url).then(response => {
+//     if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
+//     return response.json();
+//   });
+// };
+
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     // const data1 = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+//     // const data2 = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+//     // const data3 = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+//     // console.log([
+//     //   data1[0].capital[0],
+//     //   data2[0].capital[0],
+//     //   data3[0].capital[0],
+//     // ]);
+
+//     const data = await Promise.all([
+//       getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+//     ]);
+
+//     console.log(data.map(d => d[0].capital[0]));
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// get3Countries('india', 'usa', 'canada');
+
+//----------------- Promise combination race , allSettled and any -----------------
+
 const getJSON = function (url, errorMsg = 'Something went wrong') {
   return fetch(url).then(response => {
     if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
@@ -300,28 +335,49 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
   });
 };
 
-const get3Countries = async function (c1, c2, c3) {
-  try {
-    // const data1 = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
-    // const data2 = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
-    // const data3 = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/italy`),
+    getJSON(`https://restcountries.com/v3.1/name/egypt`),
+    getJSON(`https://restcountries.com/v3.1/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
 
-    // console.log([
-    //   data1[0].capital[0],
-    //   data2[0].capital[0],
-    //   data3[0].capital[0],
-    // ]);
-
-    const data = await Promise.all([
-      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
-    ]);
-
-    console.log(data.map(d => d[0].capital[0]));
-  } catch (err) {
-    console.error(err);
-  }
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request timeout'));
+    }, sec * 1000);
+  });
 };
 
-get3Countries('india', 'usa', 'canada');
+Promise.race([getJSON(`https://restcountries.com/v3.1/name/italy`), timeout(2)])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+//------------ Promise.allSettled ------------
+
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+//------------ Promise.any [ES2021] ------------
+
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('ERROR'),
+  Promise.resolve('Another success'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
