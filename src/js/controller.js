311820dc474d726cc04e44cd1229e02503d8dd0e
jeromos55 <jeromos55@gmail.com>
2024-06-17 09:30:52 +0200

    update

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,6 +8,34 @@ const timeout = function (s) {
   });
 };
 
+/*
+
+Get recipe/Delete recipe
+Allowed methods: GET DELETE
+
+GET: Returns a single recipe
+
+DELETE: Deletes a single recipe associated with provided API key
+
+Path:https://forkify-api.herokuapp.com/api/v2/recipes/:id
+
+*/
+
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/ba4845ba-0baa-4f82-adf0-ce682462553a'
+    );
+    const data = await res.json();
+
+    console.log(res, data);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
