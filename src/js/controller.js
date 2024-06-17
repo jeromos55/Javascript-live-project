c77495f4db9d40c9247abff5259fe46857b90f8e
jeromos55 <jeromos55@gmail.com>
2024-06-17 13:09:27 +0200

    update

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,11 +28,26 @@ Path:https://forkify-api.herokuapp.com/api/v2/recipes/:id
 const showRecipe = async function () {
   try {
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/ba4845ba-0baa-4f82-adf0-ce682462553a'
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
     );
     const data = await res.json();
 
-    console.log(res, data);
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    // console.log(res, data);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      serving: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
   } catch (err) {
     alert(err);
   }
