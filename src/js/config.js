c5c3d26eb443753de2ff08ffc1d4ae9e23277151
jeromos55 <jeromos55@gmail.com>
2024-07-02 11:31:49 +0200

    build

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+// export const KEY = '<YOUR_KEY>';
+export const KEY = 'dafcd8b2-f18c-4626-8f62-3361d36346c9';
 export const MODAL_CLOSE_SEC = 2.5;
